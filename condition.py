07fd0e184e90308be11f643fe3bf26c437b985f4
Hailey <vpfpt@hotmail.com>
2024-01-22 21:50:37 -0600

    Create condition.py
    컨디션파일추기

---
+++ condition.py
@@ -0,0 +1,6 @@
+famage = {"evelyn":"10", "allison":"6", "hailey":"36", "john":"40"}
+famage["daisy"] = "2"
+famage["snowball"] = "13"
+famage["hammy"] = "1"
+print(famage["hammy"])
+input("family name: ")
